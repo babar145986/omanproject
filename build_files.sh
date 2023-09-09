f59768eed59beba7c93681a3e83f132c0826a8ab
Programmer Khan <58971938+babar145986@users.noreply.github.com>
2023-09-09 16:13:13 +0500

    again updated

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
-
 echo "BUILD START"
 
-# Install project dependencies using the specified Python version
-python3.11 -m pip install -r requirements.txt
+python3.11.2 -m pip install -r requirements.txt
 
-# Collect static files for your Django project
-python3.11 manage.py collectstatic --noinput --clear
+python3.11.2 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
